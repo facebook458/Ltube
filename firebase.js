e0da0c83dc7144f30f4b525770b8e3ba7062306d
facebook458 <laxmimoyofficial@gmail.com>
2026-04-30 16:17:53 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase-config.js
+8// firebase-config.js
 // Firebase Configuration for EarningContent Platform
 
 import { initializeApp } from "firebase/app";
@@ -6,14 +6,17 @@ import { getAuth, setPersistence, browserLocalPersistence } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+
   // IMPORTANT: Replace with your Firebase project credentials
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  const firebaseConfig = {
+  apiKey: "AIzaSyCptzsy7zjlv-a3BJPPEdWl9YIeXe2Oq4k",
+  authDomain: "ltube-7d363.firebaseapp.com",
+  databaseURL: "https://ltube-7d363-default-rtdb.firebaseio.com",
+  projectId: "ltube-7d363",
+  storageBucket: "ltube-7d363.firebasestorage.app",
+  messagingSenderId: "787970159703",
+  appId: "1:787970159703:web:052f99ab131a4edcb25f12",
+  measurementId: "G-4YS2XCC6KC"
 };
 
 // Initialize Firebase
